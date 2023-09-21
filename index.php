aac521864c910e34279817796272956d1731ffca
João Marcio <127902520+JMAdF@users.noreply.github.com>
2023-09-21 19:55:11 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+
+    <meta charset="UTF-8">
+    <title>Prática 27</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="caixa0">
+        <span id="logo"><img src="logo.png"></span>
+    </div>
+    <div class="caixa1">
+        <h2>CALCULADORA DE NÚMEROS PRIMOS</h2>
+        <br>
+    <form method="post">
+        <p><u>É um número primo?</u></p> <input type="number" name="num" min="0">
+        <button class="botao">Confirmar</button> <br><br>
+     </form>
+     <p>
+    <?php
+
+    
+    ?>
+ </p>
+ 
+</div>
+</body>
+</html>
